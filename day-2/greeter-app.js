d30357695897bb7b35ecc8495722fec904321f16
tylermartin4112-a11y <tylermartin4112@gmail.com>
2026-09-23 14:39:28 -0500

    scratch day 3

--- day-2/greeter-app.js
+++ day-2/greeter-app.js
@@ -31,7 +31,19 @@
 
 function greeter(hour) {
   // YOUR CODE HERE
-}
+  if (hour >= 0 && hour <= 11){
+    console.log('Good Morning!');
+  } else if (hour >= 12 && hour <= 16) {
+    console.log("Good Afternoon!");
+  } else if (hour >= 17 && hour <= 21) {
+    console.log('Good Evening!');
+    } else if (hour >= 22 && hour <= 24) {
+      console.log('Good Night!');
+    } else{
+      console.log('Invalid Hour');
+    }
+  }
+
 
 /*
  * To test our greeter when developing, we need to fake some input data, ie,
